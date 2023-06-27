79d2aa193d202d815a879e93395f99e1766bc2a1
Cbayles <cbayles@outlook.com>
2023-06-27 08:21:59 -0400

    added tables

--- inventory.sql
+++ inventory.sql
@@ -1,17 +1,44 @@
-CREATE TABLE player {
+--ITEMS TABLE FOR ITEMS
+CREATE TABLE items (
+    item_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    description VARCHAR(255),
+);
 
-};
+--INVENTORY TABLE REPRESENTING PLAYERS INVENTORY
+CREATE TABLE inventory (
+    inventory_id INT PRIMARY KEY,
+    player_id INT, --Foreign key to reference the player
+    item_id INT, --Foreign key to reference the item
+    quantity INT,
+    FOREIGN KEY (player_id) REFERENCES Players(player_id)
+    FOREIGN KEY (item_id) REFERENCES Items(item_id)
+);
 
-CREATE TABLE playerInventory (
+--PLAYERS TABLE TO STORE PLAYER INFORMATION
+CREATE TABLE players (
+    player_id INT PRIMARY KEY,
+    name CARCHAR(50),
 
 );
 
-CREATE TABLE ItemType (
+--item insertion
+INSERT INTO items (item_id, name, description)
+VALUES (1, 'Short Sword', 'A short sword crafted by the likes of men. Not amazing, but gets the job done.')
 
-);
+INSERT INTO items (item_id, name, description)
+VALUES (2, 'Battle Axe', 'An axe crafted by the orcs. Filled with furry and hate. A weapon well feared.')
 
--- Acesss Information about complete inventory
-SELECT ItemType.Name, PlayerInventory.Amount
-From PlayerInventory
-Join ItemType On PlayerInventory.ItemType = ItemType.Id
-WHERE PlayerInventory.Playerid = ?
+INSERT INTO items (item_id, name, description)
+VALUES (3, 'Orcish Chest Plate', 'Unrefined plate armor made from the average orc blacksmith.')
+
+
+--item to player to inventory insertion
+INSERT INTO inventory (inventory_id, player_id, item_id, quantity)
+VALUES (1, 1, 1, 1) -- Adding Sword to players inventory w/ qty of 1
+
+INSERT INTO inventory (inventory_id, player_id, item_id, quantity)
+VALUES (1, 1, 2, 1)
+
+INSERT INTO inventory (inventory_id, player_id, item_id, quantity)
+VALUES (1, 1, 3, 1)
